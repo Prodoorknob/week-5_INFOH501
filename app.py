e205c21d7c31b36235b0d1ad57f30b1a909049da
Rajashekar Reddy Vedire <55252270+Prodoorknob@users.noreply.github.com>
2025-09-25 19:21:36 -0400

    Update app.py
    Added questions

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/mai
 
 st.write(
 '''
-# Titanic Visualization 1
+How do Titanic survival rates vary by passenger sex and class across different age bands?
 
 '''
 )
@@ -17,7 +17,7 @@ st.plotly_chart(fig1, use_container_width=True)
 
 st.write(
 '''
-# Titanic Visualization 2
+How is survival related to traveling alone vs. with family, and how does survival rate change as family size increases?
 '''
 )
 # Generate and display the figure
@@ -26,7 +26,7 @@ st.plotly_chart(fig2, use_container_width=True)
 
 st.write(
 '''
-# Titanic Visualization Bonus
+Which family-size groups had the highest (and lowest) survival rates on the Titanic? 
 '''
 )
 # Generate and display the figure
